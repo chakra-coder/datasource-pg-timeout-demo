f1d238d8ce2eb3311905e4f85f4543f2aa5cc094
patterncat <patterncat@gmail.com>
2017-02-10 14:33:40 +0800

    init

---
+++ src/main/java/com/xixicat/DatasourcePgTimeoutDemoApplication.java
@@ -0,0 +1,12 @@
+package com.xixicat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasourcePgTimeoutDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasourcePgTimeoutDemoApplication.class, args);
+	}
+}
